af72a8e5371d95bda76e1915b997d4cdfc350f96
Arpit Maheshwari <arp19690@gmail.com>
2016-01-04 22:31:04 +0530

    changes in the way featured posts were fetched

--- application/controllers/index.php
+++ application/controllers/index.php
@@ -12,7 +12,7 @@ class Index extends CI_Controller
     public function index()
     {
         $data = array();
-        $featured_trip_keys = $this->redis_functions->get_featured_trips();
+        $featured_trip_keys = $this->redis_functions->get_featured_trips('p.post_url_key', '0, 12');
 
         $page_title = $this->redis_functions->get_site_setting('SITE_TITLE');
         $data["featured_trip_keys"] = $featured_trip_keys;
