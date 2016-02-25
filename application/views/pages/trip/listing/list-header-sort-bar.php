a1340d196459577d34355bf1a231afa4ce464656
Arpit Maheshwari <arp19690@gmail.com>
2016-02-25 18:40:59 +0530

    jquery to redirect on changing sort filter

--- application/views/pages/trip/listing/list-header-sort-bar.php
+++ application/views/pages/trip/listing/list-header-sort-bar.php
@@ -4,7 +4,7 @@ $current_base_url = explode('?', current_url())[0];
 <div class="catalog-head fly-in">
     <label>Sort results by:</label>
     <div class="search-select">
-        <select name="sort">
+        <select name="sort" class="sort-filter-select">
             <option value="<?php echo modify_url($current_base_url, array('sort' => 'title')); ?>">Title</option>
             <option value="<?php echo modify_url($current_base_url, array('sort' => 'price_low')); ?>">Price: Low to High</option>
             <option value="<?php echo modify_url($current_base_url, array('sort' => 'price_high')); ?>">Price High to Low</option>
@@ -16,3 +16,11 @@ $current_base_url = explode('?', current_url())[0];
     <a class="show-thumbs chosen" href="#"></a> 
     <div class="clear"></div>
 </div>
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('.sort-filter-select').change(function () {
+            var sort_url = $(this).val();
+            window.location.href = sort_url;
+        });
+    });
+</script>
