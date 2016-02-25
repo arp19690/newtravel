2f6cc674b3590692da6e4dc33bc805f78158bd78
Arpit Maheshwari <arp19690@gmail.com>
2016-02-25 17:34:52 +0530

    displaying latest posts on the footer

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -1,16 +1,10 @@
 <?php
 $redis_functions = new Redisfunctions();
 $featured_trips = $redis_functions->get_featured_trips();
+$latest_trips = $redis_functions->get_latest_trips();
 ?>
 <footer class="footer-a">
     <div class="wrapper-padding">
-        <div class="section">
-            <div class="footer-lbl">Company</div>
-            <div class="footer-adress">Address: 58911 Lepzig Hore,<br />85000 Vienna, Austria</div>
-            <div class="footer-phones">Telephones: +1 777 55-32-21</div>
-            <div class="footer-email">E-mail: contacts@miracle.com</div>
-            <div class="footer-skype">Skype: angelotours</div>
-        </div>
         <div class="section">
             <div class="footer-lbl">Featured trips</div>
             <div class="footer-tours">
@@ -20,6 +14,47 @@ $featured_trips = $redis_functions->get_featured_trips();
                 {
                     $i = 0;
                     foreach ($featured_trips as $post_url_key)
+                    {
+                        $trip_details = $redis_functions->get_trip_details($post_url_key);
+                        $trip_title = stripslashes($trip_details['post_title']);
+                        $trip_description = getNWordsFromString(stripslashes($trip_details['post_description']), 20);
+                        $trip_primary_image = base_url(getImage($trip_details['post_primary_image']));
+                        $trip_url = getTripUrl($trip_details['post_url_key']);
+                        $trip_total_cost = get_currency_symbol($trip_details['post_currency']) . $trip_details['post_total_cost'];
+                        ?>
+                        <div class="footer-tour">
+                            <div class="footer-tour-l">
+                                <a href="<?php echo $trip_url; ?>">
+                                    <img alt="<?php echo $trip_title; ?>" src="<?php echo $trip_primary_image; ?>" />
+                                </a>
+                            </div>
+                            <div class="footer-tour-r">
+                                <div class="footer-tour-a"><?php echo $trip_title; ?></div>
+                                <div class="footer-tour-b"><?php echo $trip_description; ?></div>
+                                <div class="footer-tour-c"><?php echo $trip_total_cost; ?></div>
+                            </div>
+                            <div class="clear"></div>
+                        </div>
+                        <?php
+                        $i++;
+                        if ($i > 4)
+                        {
+                            break;
+                        }
+                    }
+                }
+                ?>
+            </div>
+        </div>
+        <div class="section">
+            <div class="footer-lbl">Latest trips</div>
+            <div class="footer-tours">
+                <!-- // -->
+                <?php
+                if (!empty($latest_trips))
+                {
+                    $i = 0;
+                    foreach ($latest_trips as $post_url_key)
                     {
                         $trip_details = $redis_functions->get_trip_details($post_url_key);
                         $trip_title = stripslashes($trip_details['post_title']);
