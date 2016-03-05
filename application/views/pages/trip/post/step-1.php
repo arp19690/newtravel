614663903c3e4f4d8e11f19af71b6a7ad97bad43
Arpit Maheshwari <arp19690@gmail.com>
2016-03-05 22:05:52 +0530

    post step 1, if the form values exists, pre-populate them

--- application/views/pages/trip/post/step-1.php
+++ application/views/pages/trip/post/step-1.php
@@ -2,7 +2,7 @@
     <div class="body-wrapper">
         <div class="wrapper-padding">
             <div class="page-head">
-                <div class="page-title">Trip - <span><?php echo $page_title;?></span></div>
+                <div class="page-title">Trip - <span><?php echo $page_title; ?></span></div>
                 <?php
                 if (isset($breadcrumbs) && !empty($breadcrumbs))
                 {
@@ -23,11 +23,11 @@
                                         <div class="booking-form">
                                             <div class="booking-form-i">
                                                 <label>Title:</label>
-                                                <div class="input"><input type="text" name="trip_title" required="required" /></div>
+                                                <div class="input"><input type="text" name="trip_title" required="required" value="<?php echo isset($post_records['post_title']) == TRUE ? stripslashes($post_records['post_title']) : ''; ?>"/></div>
                                             </div>
                                             <div class="booking-form-i">
                                                 <label>Long Description:</label>
-                                                <div class="textarea"><textarea name="trip_description" required="required" minlength="200"></textarea></div>
+                                                <div class="textarea"><textarea name="trip_description" required="required" minlength="200"><?php echo isset($post_records['post_description']) == TRUE ? stripslashes($post_records['post_description']) : ''; ?></textarea></div>
                                             </div>						
                                         </div>
 
@@ -38,13 +38,26 @@
 
                                             <div class="booking-form">
                                                 <?php
+//                                                In case of edit, let's form another array for existing activities
+                                                $post_activity_ids = array();
+                                                if (isset($post_records['post_activities']))
+                                                {
+                                                    if (!empty($post_records['post_activities']))
+                                                    {
+                                                        foreach ($post_records['post_activities'] as $value)
+                                                        {
+                                                            $post_activity_ids[] = $value->pa_id;
+                                                        }
+                                                    }
+                                                }
+
                                                 if (isset($activity_master) && !empty($activity_master))
                                                 {
                                                     foreach ($activity_master as $key => $value)
                                                     {
                                                         ?>
                                                         <div class="checkbox">
-                                                            <label><input name="activities[]" type="checkbox" value="<?php echo $value->am_id; ?>" /><?php echo $value->am_title; ?></label>
+                                                            <label><input name="activities[]" type="checkbox" value="<?php echo $value->am_id; ?>" <?php echo in_array($value->am_id, $post_activity_ids) == TRUE ? 'checked="checked"' : ''; ?>/><?php echo $value->am_title; ?></label>
                                                         </div> 
                                                         <?php
                                                     }
