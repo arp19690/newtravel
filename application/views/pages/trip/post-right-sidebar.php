036f189f00e35a54fb1c270a4623d56756b36dd0
Arpit Maheshwari <arp19690@gmail.com>
2016-03-17 14:44:09 +0530

    adding rich snippets to the post right sidebar

--- application/views/pages/trip/post-right-sidebar.php
+++ application/views/pages/trip/post-right-sidebar.php
@@ -24,14 +24,14 @@ if (!empty($post_records['post_regions']))
     }
 }
 ?>
-<div class="checkout-coll">
+<div class="checkout-coll" itemscope itemtype="http://schema.org/Event">
     <div class="checkout-head">
-        <div class="checkout-headl">
+        <div class="checkout-headl" itemscope itemtype="http://schema.org/Product">
             <?php
             if (!empty($post_records['post_primary_image']) && file_exists($post_records['post_primary_image']))
             {
                 ?>
-                <a href="#"><img alt="<?php echo $post_title; ?>" src="<?php echo base_url($post_records['post_primary_image']); ?>"></a>
+                <a href="#"><img itemprop="image" alt="<?php echo $post_title; ?>" src="<?php echo base_url($post_records['post_primary_image']); ?>"></a>
                 <?php
             }
             ?>
@@ -40,7 +40,7 @@ if (!empty($post_records['post_regions']))
             <div class="checkout-headrb">
                 <div class="checkout-headrp">
                     <div class="chk-left">
-                        <div class="chk-lbl"><a href="#"><?php echo $post_title; ?></a></div>
+                        <div class="chk-lbl"><a href="#"><span itemprop="name"><?php echo $post_title; ?></span></a></div>
                         <div class="chk-lbl-a"><?php echo implode(' > ', $post_region_cities); ?></div>
                     </div>
 
@@ -56,12 +56,11 @@ if (!empty($post_records['post_regions']))
 
     <div class="chk-lines">
         <div class="chk-line">
+            <meta itemprop="duration" content="<?php echo $post_total_days; ?>D" />
             <span class="chk-nights"><?php echo number_format($post_total_days); ?> days</span>
             <span class="chk-dates"><?php echo $post_start_end_date_string; ?></span>
         </div>
-        <div class="chk-line">
-            1 STANDARD FAMILY ROOM FOR <span class="chk-persons">3 PERSONS</span>
-        </div>
+        <div class="chk-line" itemprop="description"><?php echo $post_description; ?></div>
     </div>
 
     <div class="chk-details">
@@ -88,9 +87,9 @@ if (!empty($post_records['post_regions']))
                 <div class="clear"></div>
             </div>
         </div>
-        <div class="chk-total">
-            <div class="chk-total-l">Total Price</div>
-            <div class="chk-total-r"><?php echo $post_total_cost; ?></div>
+        <div class="chk-total" itemscope itemtype="http://schema.org/Offer">
+            <div class="chk-total-l">Budget</div>
+            <div class="chk-total-r" itemprop="price" content="<?php echo number_format($post_records['post_total_cost'], 2); ?>"><?php echo $post_total_cost; ?></div>
             <div class="clear"></div>
         </div>					
     </div>
