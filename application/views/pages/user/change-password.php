15cdfe05627cf13ff89340674c9df2074a89382a
Arpit Maheshwari <arp19690@gmail.com>
2016-03-19 11:44:28 +0530

    change password view page complete

--- application/views/pages/user/change-password.php
+++ application/views/pages/user/change-password.php
@@ -1,37 +1,71 @@
-<div class="container margin-top-40 col-lg-6 col-lg-offset-3 col-sm-8 col-sm-offset-2">
+<div class="main-cont">	
+    <div class="inner-page">
+        <div class="inner-breadcrumbs" style="margin: 0;">
+            <div class="content-wrapper">
+                <div class="page-title">Change Password</div>
+                <?php
+                if (isset($breadcrumbs) && !empty($breadcrumbs))
+                {
+                    echo $breadcrumbs;
+                }
+                ?>
+                <div class="clear"></div>
+            </div>		
+        </div>
 
-    <div class="row margin-bottom-20 clearfix">
-        <h1>Change Password</h1>
-        <a href='javascript:history.go(-1);' class='btn btn-default pull-right clearfix'><i class='glyphicon glyphicon-arrow-left'></i>&nbsp;Back</a>
+        <div class="contacts-page-holder">
+            <div class="contacts-page">
+                <div class="contacts-colls">
+                    <div class="contacts-colls-l">
+                        <div class="clear"></div>
+                    </div>
+
+                    <div class="contacts-colls-r">
+                        <div class="contacts-colls-rb">
+                            <div class="contact-colls-lbl">Enter New Password</div>
+                            <div class="booking-form">
+                                <form id="contact_form" action="" method="post">
+                                    <div class="clear">
+                                        <div class="booking-form-i">
+                                            <label>New Password:</label>
+                                            <div class="input"><input type="password" name="new_password" placeholder="Enter new password" required="required" /></div>
+                                        </div>
+                                    </div>
+
+                                    <div class="clear">
+                                        <div class="booking-form-i">
+                                            <label>Confirm Password:</label>
+                                            <div class="input"><input type="password" name="confirm_password" placeholder="Confirm password" required="required" /></div>
+                                        </div>
+                                    </div>
+
+                                    <div class="clear form-update-btn hidden">
+                                        <button type="submit" name="btn_submit" class="contacts-send">Update</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <div class="clear"></div>
+                    </div>
+                </div>
+                <div class="clear"></div>	
+            </div>
+        </div>
     </div>
-
-    <div class="row">
-        <!--  ==========  -->
-        <!--  = Main content =  -->
-        <!--  ==========  -->
-        <section class="">
-
-            <form class="form-horizontal validate-form" method='post' action='' enctype="multipart/form-data">
-                <div class="margin-bottom-20 clearfix">
-
-                    <label for="new_password" >New Password <span class="required">*</span></label>
-                    <input class="form-control required" name="new_password" id="new_password" type="password" required="required" placeholder="New Password"/>
-
-                </div>
-                
-                <div class="margin-bottom-20 clearfix">
-
-                    <label for="confirm_password" >Confirm Password <span class="required">*</span></label>
-                    <input class="form-control required" name="confirm_password" id="confirm_password" type="password" required="required" placeholder="Change Password"/>
-
-                </div>
-
-                <div class="col-lg-12 text-center">	
-                    <input class="btn btn-primary btn-lg" name="btn_submit" value="Submit" type="submit">
-                </div>
-            </form>
-
-        </section> <!-- /main content -->
-
-    </div>
-</div> <!-- /container -->
+</div>
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('input[type="password"]').keyup(function () {
+            var new_pass = $('input[name="new_password"]').val();
+            var confirm_pass = $('input[name="confirm_password"]').val();
+            if (new_pass === confirm_pass && new_pass != null && confirm_pass != null)
+            {
+                $('.form-update-btn').removeClass('hidden');
+            }
+            else
+            {
+                $('.form-update-btn').addClass('hidden');
+            }
+        });
+    });
+</script>
