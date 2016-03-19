81f0a3f8ff914c4bf13ca0a29feb300e98a789be
Arpit Maheshwari <arp19690@gmail.com>
2016-03-19 17:17:50 +0530

    adding login/register with facebook buttons

--- application/views/layout/authorize-popups.php
+++ application/views/layout/authorize-popups.php
@@ -8,27 +8,41 @@
     </div>
     <section class="autorize-tab-content">
         <div class="autorize-padding">
-            <form action="<?php echo base_url('login?next=' . current_url()); ?>" method="POST">
-                <h6 class="autorize-lbl">Welcome! Login to Your Account</h6>
-                <input type="email" name="user_email" required="required" placeholder="Email"/>
-                <input type="password" name="user_password" required="required" placeholder="Password"/>
-                <footer class="autorize-bottom">
-                    <button class="authorize-btn">Login</button>
-                    <a href="<?php echo base_url('forgot-password'); ?>" class="authorize-forget-pass">Forgot your password?</a>
-                    <div class="clear"></div>
-                </footer>
-            </form>
+            <div class="clear text-center">
+                <a href="<?php echo base_url('facebook-login'); ?>" class="btn facebook-btn"><span class="fa fa-facebook"></span>&nbsp;&nbsp;|&nbsp;&nbsp;Login with Facebook</a>
+                <p style="margin: 10px 0;"><small>or,</small></p>
+            </div>
+
+            <div class="clear">
+                <form action="<?php echo base_url('login?next=' . current_url()); ?>" method="POST">
+                    <h6 class="autorize-lbl">Welcome! Login to Your Account</h6>
+                    <input type="email" name="user_email" required="required" placeholder="Email"/>
+                    <input type="password" name="user_password" required="required" placeholder="Password"/>
+                    <footer class="autorize-bottom">
+                        <button class="authorize-btn">Login</button>
+                        <a href="<?php echo base_url('forgot-password'); ?>" class="authorize-forget-pass">Forgot your password?</a>
+                        <div class="clear"></div>
+                    </footer>
+                </form>
+            </div>
         </div>
     </section>
     <section class="autorize-tab-content">
         <div class="autorize-padding">
-            <h6 class="autorize-lbl">Register for Your Account</h6>
-            <input type="email" name="user_email" required="required" placeholder="Email"/>
-            <input type="password" name="user_password" required="required" placeholder="Password"/>
-            <footer class="autorize-bottom">
-                <button class="authorize-btn">Register</button>
-                <div class="clear"></div>
-            </footer>
+            <div class="clear text-center">
+                <a href="<?php echo base_url('facebook-login'); ?>" class="btn facebook-btn"><span class="fa fa-facebook"></span>&nbsp;&nbsp;|&nbsp;&nbsp;Register with Facebook</a>
+                <p style="margin: 10px 0;"><small>or,</small></p>
+            </div>
+
+            <div class="clear">
+                <h6 class="autorize-lbl">Register for Your Account</h6>
+                <input type="email" name="user_email" required="required" placeholder="Email"/>
+                <input type="password" name="user_password" required="required" placeholder="Password"/>
+                <footer class="autorize-bottom">
+                    <button class="authorize-btn">Register</button>
+                    <div class="clear"></div>
+                </footer>
+            </div>
         </div>
     </section>
 </div>
--- application/views/pages/index/register.php
+++ application/views/pages/index/register.php
@@ -26,36 +26,45 @@
                         <div class="contacts-colls-rb">
                             <div class="contact-colls-lbl"><?php echo $page_title; ?></div>
                             <div class="booking-form">
-                                <form id="contact_form" action="<?php echo base_url('register?next=' . current_url()); ?>" method="post">
-                                    <div class="booking-form-i">
-                                        <label>Full Name:</label>
-                                        <div class="input"><input type="text" name="user_fullname" placeholder="Enter your full name" required="required" /></div>
+                                <div class="clear">
+                                    <div class="text-center">
+                                        <a href="<?php echo base_url('facebook-login'); ?>" class="btn facebook-btn"><span class="fa fa-facebook"></span>&nbsp;&nbsp;|&nbsp;&nbsp;Register with Facebook</a>
+                                        <p style="margin: 10px 0;"><small>or,</small></p>
                                     </div>
-                                    <div class="booking-form-i">
-                                        <label>Your city:</label>
-                                        <div class="input"><input type="text" name="user_location" placeholder="Enter your city" class="gMapLocation-cities" /></div>
-                                    </div>
-                                    <div class="booking-form-i">
-                                        <label>Email:</label>
-                                        <div class="input"><input type="email" name="user_email" placeholder="Enter your email" required="required" /></div>
-                                    </div>
-                                    <div class="booking-form-i">
-                                        <label>Password:</label>
-                                        <div class="input"><input type="password" name="user_password" placeholder="Enter your password" required="required" /></div>
-                                    </div>
-                                    <div class="booking-form-i">
-                                        <input type="hidden" name="user_gender" class="user_gender_input" value="male"/>
-                                        <div class="form-sex">
-                                            <label>Male/Female:</label>
-                                            <div class="sex-type chosen" data-value="male">M</div>
-                                            <div class="sex-type" data-value="female">F</div>
-                                            <div class="clear"></div>
+                                </div>
+
+                                <div class="clear">
+                                    <form id="contact_form" action="<?php echo base_url('register?next=' . current_url()); ?>" method="post">
+                                        <div class="booking-form-i">
+                                            <label>Full Name:</label>
+                                            <div class="input"><input type="text" name="user_fullname" placeholder="Enter your full name" required="required" /></div>
                                         </div>
-                                    </div>
-                                    <div class="clear"></div>
-                                    <button type="submit" class="contacts-send">Register</button>
-                                    <a href="<?php echo base_url('login'); ?>" style="margin-left: 25px;" class="a-no-underline">Already have an account?</a>
-                                </form>
+                                        <div class="booking-form-i">
+                                            <label>Your city:</label>
+                                            <div class="input"><input type="text" name="user_location" placeholder="Enter your city" class="gMapLocation-cities" /></div>
+                                        </div>
+                                        <div class="booking-form-i">
+                                            <label>Email:</label>
+                                            <div class="input"><input type="email" name="user_email" placeholder="Enter your email" required="required" /></div>
+                                        </div>
+                                        <div class="booking-form-i">
+                                            <label>Password:</label>
+                                            <div class="input"><input type="password" name="user_password" placeholder="Enter your password" required="required" /></div>
+                                        </div>
+                                        <div class="booking-form-i">
+                                            <input type="hidden" name="user_gender" class="user_gender_input" value="male"/>
+                                            <div class="form-sex">
+                                                <label>Male/Female:</label>
+                                                <div class="sex-type chosen" data-value="male">M</div>
+                                                <div class="sex-type" data-value="female">F</div>
+                                                <div class="clear"></div>
+                                            </div>
+                                        </div>
+                                        <div class="clear"></div>
+                                        <button type="submit" class="contacts-send">Register</button>
+                                        <a href="<?php echo base_url('login'); ?>" style="margin-left: 25px;" class="a-no-underline">Already have an account?</a>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                         <div class="clear"></div>
