0bb730fcc6a5d22f9272851b75ec2d0af99c7bc2
Arpit Maheshwari <arp19690@gmail.com>
2016-04-05 23:34:28 +0530

    adding you may like to another file and calling it

--- application/views/pages/trip/trip-detail.php
+++ application/views/pages/trip/trip-detail.php
@@ -226,58 +226,7 @@ if (!empty($post_details['post_regions']))
                     <?php
                     if (!empty($post_details['you_may_like']))
                     {
-                        ?>
-                        <div class="h-liked">
-                            <div class="h-liked-lbl">You May Also Like</div>
-                            <div class="h-liked-row">
-                                <?php
-                                foreach ($post_details['you_may_like'] as $ykey => $yvalue)
-                                {
-                                    $post_title = stripslashes($yvalue['post_title']);
-                                    $post_primary_image = base_url(getImage($yvalue['post_primary_image']));
-                                    $post_url = getTripUrl($yvalue['post_url_key']);
-                                    $post_total_cost = get_currency_symbol($yvalue['post_currency']) . $yvalue['post_total_cost'];
-                                    ?>
-                                    <!-- // -->
-                                    <div class="h-liked-item">
-                                        <div class="h-liked-item-i">
-                                            <div class="h-liked-item-l">
-                                                <a href="<?php echo $post_url; ?>"><img alt="<?php echo $post_title; ?>" src="<?php echo $post_primary_image; ?>"></a>
-                                            </div>
-                                            <div class="h-liked-item-c">
-                                                <div class="h-liked-item-cb">
-                                                    <div class="h-liked-item-p">
-                                                        <div class="h-liked-title"><a href="<?php echo $post_url; ?>"><?php echo $post_title; ?></a></div>
-                                                        <div class="h-liked-rating">
-                                                            <nav class="stars">
-                                                                <ul>
-                                                                    <li><a href="#"><img alt="" src="<?php echo IMAGES_PATH; ?>/star-b.png" /></a></li>
-                                                                    <li><a href="#"><img alt="" src="<?php echo IMAGES_PATH; ?>/star-b.png" /></a></li>
-                                                                    <li><a href="#"><img alt="" src="<?php echo IMAGES_PATH; ?>/star-b.png" /></a></li>
-                                                                    <li><a href="#"><img alt="" src="<?php echo IMAGES_PATH; ?>/star-b.png" /></a></li>
-                                                                    <li><a href="#"><img alt="" src="<?php echo IMAGES_PATH; ?>/star-a.png" /></a></li>
-                                                                </ul>
-                                                                <div class="clear"></div>
-                                                            </nav>
-                                                        </div>
-                                                        <div class="h-liked-foot">
-                                                            <span class="h-liked-price"><?php echo $post_total_cost; ?></span>
-                                                            <span class="h-liked-comment">Budget</span>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="clear"></div>
-                                            </div>
-                                        </div>
-                                        <div class="clear"></div>	
-                                    </div>
-                                    <!-- \\ -->
-                                    <?php
-                                }
-                                ?>
-                            </div>			
-                        </div>
-                        <?php
+                        $this->load->view('pages/trip/you-may-like', array('you_may_like_records' => $post_details['you_may_like']));
                     }
                     ?>
                 </div>
