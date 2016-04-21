b8b2eb066889d8bd22d0d0ed5e3b546340af5dff
Arpit Maheshwari <arp19690@gmail.com>
2016-04-21 16:59:19 +0530

    displaying post faqs to the trip details page

--- application/views/pages/trip/trip-detail-faq.php
+++ application/views/pages/trip/trip-detail-faq.php
@@ -1,79 +1,32 @@
+<?php
+$redis_functions = new Redisfunctions();
+$trip_faqs = $redis_functions->get_trip_faqs();
+?>
 <!-- // content-tabs-i // -->
 <div class="content-tabs-i">
     <h2>FAQ</h2>
-    <p class="small">Voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui voluptatem sequi nesciunt. </p>
+    <p class="small">Most frequently asked questions by backpackers about trips posted on our platform.</p>
     <div class="todo-devider"></div>
     <div class="faq-row">
-        <!-- // -->
-        <div class="faq-item">
-            <div class="faq-item-a">
-                <span class="faq-item-left">Totam rem aperiam, eaquie ipsa quae?</span>
-                <span class="faq-item-i"></span>
-                <div class="clear"></div>
-            </div>
-            <div class="faq-item-b">
-                <div class="faq-item-p">
-                    Voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia aspernatur aut odit aut fugit consequuntur magni dolores eos qui voluptatem sequi nesciunt. aspernatur aut odit aut fugit  
+        <?php
+        foreach ($trip_faqs as $value)
+        {
+            ?>
+            <!-- // -->
+            <div class="faq-item">
+                <div class="faq-item-a">
+                    <span class="faq-item-left"><?php echo stripslashes($value['faq_question']); ?></span>
+                    <span class="faq-item-i"></span>
+                    <div class="clear"></div>
+                </div>
+                <div class="faq-item-b">
+                    <div class="faq-item-p"><?php echo stripslashes($value['faq_answer']); ?></div>
                 </div>
             </div>
-        </div>
-        <!-- \\ -->
-        <!-- // -->
-        <div class="faq-item">
-            <div class="faq-item-a">
-                <span class="faq-item-left">Dolores eos qui ratione voluptatem sequi nescuin?</span>
-                <span class="faq-item-i"></span>
-                <div class="clear"></div>
-            </div>
-            <div class="faq-item-b">
-                <div class="faq-item-p">
-                    Voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia aspernatur aut odit aut fugit consequuntur magni dolores eos qui voluptatem sequi nesciunt. aspernatur aut odit aut fugit  
-                </div>
-            </div>
-        </div>
-        <!-- \\ -->
-        <!-- // -->
-        <div class="faq-item">
-            <div class="faq-item-a">
-                <span class="faq-item-left">Neque porro quisquam est, qui dolorem ipsum?</span>
-                <span class="faq-item-i"></span>
-                <div class="clear"></div>
-            </div>
-            <div class="faq-item-b">
-                <div class="faq-item-p">
-                    Voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia aspernatur aut odit aut fugit consequuntur magni dolores eos qui voluptatem sequi nesciunt. aspernatur aut odit aut fugit  
-                </div>
-            </div>
-        </div>
-        <!-- \\ -->
-        <!-- // -->
-        <div class="faq-item">
-            <div class="faq-item-a">
-                <span class="faq-item-left">Dolor sit amet consectutur adipisci velit, sed?</span>
-                <span class="faq-item-i"></span>
-                <div class="clear"></div>
-            </div>
-            <div class="faq-item-b">
-                <div class="faq-item-p">
-                    Voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia aspernatur aut odit aut fugit consequuntur magni dolores eos qui voluptatem sequi nesciunt. aspernatur aut odit aut fugit  
-                </div>
-            </div>
-        </div>
-        <!-- \\ -->
-        <!-- // -->
-        <div class="faq-item">
-            <div class="faq-item-a">
-                <span class="faq-item-left">Consectetur, adipisci velit, sed quia non numquam?</span>
-                <span class="faq-item-i"></span>
-                <div class="clear"></div>
-            </div>
-            <div class="faq-item-b">
-                <div class="faq-item-p">
-                    Voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia aspernatur aut odit aut fugit consequuntur magni dolores eos qui voluptatem sequi nesciunt. aspernatur aut odit aut fugit  
-                </div>
-            </div>
-        </div>
-        <!-- \\ -->
+            <!-- \\ -->
+            <?php
+        }
+        ?>
     </div>
 </div>
 <!-- \\ content-tabs-i \\ -->
