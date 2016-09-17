1cdba0ec581e494cf620b2120eedea5e84aa4aff
Arpit Maheshwari <arp19690@gmail.com>
2016-09-17 12:33:15 +0530

    bug fix

--- application/views/layout/admin/header.php
+++ application/views/layout/admin/header.php
@@ -9,7 +9,7 @@ $redis_functions = new Redisfunctions();
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Admin Panel - <?php echo SITE_NAME; ?></title>
+        <title>Admin Panel - <?php echo $redis_functions->get_site_setting('SITE_NAME'); ?></title>
 
         <!-- Global stylesheets -->
         <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
