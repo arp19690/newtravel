00f3ada16e8ca89a01f05202876d9a76c530c10e
Arpit Maheshwari <arp19690@gmail.com>
2016-11-09 21:38:50 +0530

    commenting pagination as of now

--- application/views/pages/trip/listing/pagination.php
+++ application/views/pages/trip/listing/pagination.php
@@ -1,6 +1,6 @@
-<div class="pagination">
+<!--<div class="pagination">
     <a class="active" href="#">1</a>
     <a href="#">2</a>
     <a href="#">3</a>
     <div class="clear"></div>
-</div> 
+</div> -->
