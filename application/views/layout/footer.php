5c8b64c040ef37b3bf9c786b15f0cc155e77cd5b
Arpit Maheshwari <arp19690@gmail.com>
2017-02-03 10:23:50 +0530

    display ad in footer

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -1,51 +1,10 @@
 <?php
 $redis_functions = new Redisfunctions();
-$featured_trips = $redis_functions->get_featured_trips();
+//$featured_trips = $redis_functions->get_featured_trips();
 $latest_trips = $redis_functions->get_latest_trips();
 ?>
 <footer class="footer-a">
     <div class="wrapper-padding">
-        <div class="section">
-            <div class="footer-lbl">Featured trips</div>
-            <div class="footer-tours">
-                <!-- // -->
-                <?php
-                if (!empty($featured_trips))
-                {
-                    $i = 0;
-                    foreach ($featured_trips as $post_url_key)
-                    {
-                        $trip_details = $redis_functions->get_trip_details($post_url_key['post_url_key']);
-                        $trip_title = stripslashes($trip_details['post_title']);
-                        $trip_description = getNWordsFromString(stripslashes($trip_details['post_description']), 20);
-                        $trip_primary_image = base_url(getImage($trip_details['post_primary_image']));
-                        $trip_url = getTripUrl($trip_details['post_url_key']);
-                        $trip_total_cost = get_currency_symbol($trip_details['post_currency']) . $trip_details['post_total_cost'];
-                        ?>
-                        <div class="footer-tour">
-                            <div class="footer-tour-l">
-                                <a href="<?php echo $trip_url; ?>">
-                                    <img alt="<?php echo $trip_title; ?>" src="<?php echo $trip_primary_image; ?>" />
-                                </a>
-                            </div>
-                            <div class="footer-tour-r">
-                                <div class="footer-tour-a"><?php echo $trip_title; ?></div>
-                                <div class="footer-tour-b"><?php echo $trip_description; ?></div>
-                                <div class="footer-tour-c"><?php echo $trip_total_cost; ?></div>
-                            </div>
-                            <div class="clear"></div>
-                        </div>
-                        <?php
-                        $i++;
-                        if ($i > 4)
-                        {
-                            break;
-                        }
-                    }
-                }
-                ?>
-            </div>
-        </div>
         <div class="section">
             <div class="footer-lbl">Latest trips</div>
             <div class="footer-tours">
@@ -116,6 +75,12 @@ $latest_trips = $redis_functions->get_latest_trips();
                 <?php
             }
             ?>
+        </div>
+        <div class="section">
+            <div class="footer-lbl"></div>
+            <div class="twitter-wiget">
+                <div class="clearfix"><?php echo get_google_ad(); ?></div>
+            </div>
         </div>
     </div>
     <div class="clear"></div>
